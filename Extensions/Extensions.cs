d14562f74ced08a7a4517464cb51470e9f065af7
Zachary Zak <zachzak@hotmail.com>
2025-10-14 13:12:42 -0500

    Add delete confirmations

---
+++ Extensions/Extensions.cs
@@ -0,0 +1,18 @@
+﻿using MudBlazor;
+
+namespace FitTrack.Extensions;
+
+public static class Extensions
+{
+    public async static Task<bool> ConfirmDelete(this IDialogService dialogService)
+    {
+        bool? result = await dialogService.ShowMessageBox("Warning", "Deleting can not be undone!", yesText: "Delete!", cancelText: "Cancel");
+
+        if (result.HasValue)
+        {
+            return result.Value;
+        }
+
+        return false;
+    }
+}

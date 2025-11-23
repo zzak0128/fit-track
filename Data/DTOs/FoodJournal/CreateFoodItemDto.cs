1fea8096d7ec5a0e467c8d5b90c5ad5bfd86b2b9
Zachary Zak <zachzak@hotmail.com>
2025-11-23 17:47:31 -0600

    Update exercise Grid view layout

--- Data/DTOs/FoodJournal/CreateFoodItemDto.cs
+++ Data/DTOs/FoodJournal/CreateFoodItemDto.cs
@@ -8,11 +8,11 @@ public class CreateFoodItemDto
 
     public string Units { get; set; } = "Each";
 
-    public int Calories { get; set; }
+    public double Calories { get; set; }
 
-    public int Carbs { get; set; }
+    public double Carbs { get; set; }
 
-    public int Fats { get; set; }
+    public double Fats { get; set; }
 
-    public int Proteins { get; set; }
+    public double Proteins { get; set; }
 }
